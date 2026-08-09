98fc1f0892f0f23bd4d610ffd361fb77a3a3f35a
Roshan Vernekar <92033445+roshanvernekar3@users.noreply.github.com>
2026-08-09 20:18:55 +0530

    Added solution - Time: O(n log n), Space: O(n)

---
+++ 703297-largest-in-array-GFG/703297-largest-in-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def largest(self, arr):
+        arr.sort()
+        return arr[-1]
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
